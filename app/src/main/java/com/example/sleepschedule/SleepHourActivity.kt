01dfedc6818eb73e5451dbaf2abbd508dad09bc2
Huynh Gia Dat <ishii.kami2001@gmail.com>
2021-07-10 21:12:52 +0700

    Start Button Function on Screen1UI

--- app/src/main/java/com/example/sleepschedule/SleepHourActivity.kt
+++ app/src/main/java/com/example/sleepschedule/SleepHourActivity.kt
@@ -2,6 +2,7 @@ package com.example.sleepschedule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.github.stefanodp91.android.circularseekbar.CircularSeekBar
 import com.github.stefanodp91.android.circularseekbar.OnCircularSeekBarChangeListener
 import kotlinx.android.synthetic.main.activity_sleep_hour.*
@@ -38,5 +39,14 @@ class SleepHourActivity : AppCompatActivity() {
              */
             override fun onStopTrackingTouch(CircularSeekBar: CircularSeekBar) {}
         })
+
+    }
+
+    // Start button click
+    fun startCountTime(view: View) {
+        buttonStart.visibility = View.GONE
+        buttonNext.visibility = View.GONE
+        countUpTimer.start()
+        countUpTimer.visibility = View.VISIBLE
     }
 }
