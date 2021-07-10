262fc0e748c90b3df8faa0fd6b1f483ff8215017
iluvyw <anhoangpham2001@gmail.com>
2021-07-10 14:58:01 +0700

    Add Calendar Activity

---
+++ app/src/main/java/com/example/sleepschedule/CalendarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sleepschedule
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CalendarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
