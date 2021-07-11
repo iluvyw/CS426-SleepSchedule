de5c4f8b20ae9bc454510664cc094c0c472851a1
iluvyw <anhoangpham2001@gmail.com>
2021-07-11 12:02:27 +0700

    add pop_up toast

--- app/src/main/java/com/example/sleepschedule/CalendarActivity.kt
+++ app/src/main/java/com/example/sleepschedule/CalendarActivity.kt
@@ -3,8 +3,10 @@ package com.example.sleepschedule
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_calendar.*
+import kotlinx.android.synthetic.main.pop_up.view.*
 
 class CalendarActivity : AppCompatActivity(),setValue,getValue {
 
@@ -25,7 +27,47 @@ class CalendarActivity : AppCompatActivity(),setValue,getValue {
             //tvTitle.setText(generateInfoKey(date,"numSleep"))
             setImageViewColor()
             setEditTextResult()
+            popupShow()
         }
+    }
+
+    private fun popupShow(){
+        val myPopUp = layoutInflater
+        val view = myPopUp.inflate(R.layout.pop_up,null)
+
+        var result = 0
+        for (i in timeSleep.indices){
+            result += timeSleep[i]
+        }
+        var percent = 0
+        if (timeGoal>0){
+            percent = result * 100 / timeGoal
+        }
+        view.etGoalTime.setText(timeGoal.toString())
+        view.etSleepTime.setText(result.toString())
+
+        result = 0
+        for (i in timeSleep.indices){
+            result += timeSleep[i]
+        }
+        if (timeGoal == 0){
+            view.ivResult.setBackgroundColor(Color.rgb(255,255,255))
+        }
+        else if (result<(0.5*timeGoal)){
+            view.ivResult.setBackgroundColor(Color.rgb(255,0,0)) //red
+        }
+        else if (result<=(0.75*timeGoal)){
+            view.ivResult.setBackgroundColor(Color.rgb(255,255,0)) //yellow
+        }
+        else{
+            view.ivResult.setBackgroundColor(Color.rgb(0,255,0)) //green
+        }
+
+        val toast = Toast(applicationContext)
+        toast.view = view
+        toast.duration = Toast.LENGTH_SHORT
+        toast.setGravity(Gravity.CENTER_VERTICAL,0,0)
+        toast.show()
     }
 
     private fun setEditTextResult() {
