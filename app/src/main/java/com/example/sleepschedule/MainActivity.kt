02a4acd776da35f785ae8e30e1700396d6b83147
iluvyw <anhoangpham2001@gmail.com>
2021-07-14 17:57:38 +0700

    fix Toast duration and modify code

--- app/src/main/java/com/example/sleepschedule/MainActivity.kt
+++ app/src/main/java/com/example/sleepschedule/MainActivity.kt
@@ -13,27 +13,6 @@ class MainActivity : AppCompatActivity(),setValue,getValue {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btCalendar.setOnClickListener {
-            /*var timeSleep = mutableListOf<Int>(10,20,30,40)
-            var timeStartSleep = mutableListOf<Int>(1,2,3,4,5)
-            var timeGoal = mutableListOf<Int>(10,20,30,40)
-            setIntList(this, generateInfoKey("14/07/2021","timeSleep"),timeSleep)
-            setIntList(this, generateInfoKey("14/07/2021","timeStartSleep"),timeStartSleep)
-            setIntList(this, generateInfoKey("14/07/2021","timeGoal"),timeGoal)
-
-            timeSleep = mutableListOf<Int>(20,30,40,50)
-            timeStartSleep = mutableListOf<Int>(1,2,3,4,5)
-            timeGoal = mutableListOf<Int>(50,50,50,50)
-            setIntList(this, generateInfoKey("13/07/2021","timeSleep"),timeSleep)
-            setIntList(this, generateInfoKey("13/07/2021","timeStartSleep"),timeStartSleep)
-            setIntList(this, generateInfoKey("13/07/2021","timeGoal"),timeGoal)
-
-            timeSleep = mutableListOf<Int>(30,20,10,40)
-            timeStartSleep = mutableListOf<Int>(1,2,3,4,5)
-            timeGoal = mutableListOf<Int>(100,1000,100,100)
-            setIntList(this, generateInfoKey("12/07/2021","timeSleep"),timeSleep)
-            setIntList(this, generateInfoKey("12/07/2021","timeStartSleep"),timeStartSleep)
-            setIntList(this, generateInfoKey("12/07/2021","timeGoal"),timeGoal)*/
-
             val intent = Intent(this,CalendarActivity::class.java)
             startActivity(intent)
         }
